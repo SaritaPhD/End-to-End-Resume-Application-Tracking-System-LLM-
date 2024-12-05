b1e69cdb9f2b94e37d8774ad68f4263e147c4356
Sarita <126271555+SaritaPhD@users.noreply.github.com>
2024-12-05 11:32:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ description:{jd}
 
 I want the response in one single string having the structure
 {{"JD Match":"%","MissingKeywords:[]","Profile Summary":""}}
-"""
+""" 
 
 # Streamlit app
 st.title("Application Tracking System (ATS)")
